99ad9be6f54addac5877ccd2c845262a4cbaa73f
fqrmix <90894198+fqrmix@users.noreply.github.com>
2022-02-15 16:13:39 +0300

    Update main.js
    added: data by id

--- main.js
+++ main.js
@@ -8,23 +8,23 @@
 		{
 			id: 1,
 			name: 'Причина в Кассе',
-			body: 'Payload: &quot;uniLabel&quot; && Payload: (&quot;warn&quot; &quot;error&quot; &quot;auth check unsuccessful&quot; &quot;restrictionid&quot;) \
-			&& NOT Payload: (&quot;error=null&quot; &quot;errors=null&quot; &quot;error=optional&quot; &quot;error=optional.empty&quot; \
-			&quot;skip loyalty-gate request&quot; &quot;rollbackpayment&quot;)',
+			body: 'Payload: "uniLabel" && Payload: ("warn" "error" "auth check unsuccessful" "restrictionid") \
+			&& NOT Payload: ("error=null" "errors=null" "error=optional" "error=optional.empty" \
+			"skip loyalty-gate request" "rollbackpayment")',
 			module: 'backend-kassa-main'
 		},
 		
 		{
 			id: 2,
 			name: 'Ответ в шопах',
-			body: 'Payload: &quot;TraceID&quot; && Payload: (&quot;create-order-refused&quot; &quot;payment approval error&quot;)',
+			body: 'Payload: "TraceID" && Payload: ("create-order-refused" "payment approval error")',
 			module: 'backend-shop-main'
 		},
 		
 		{
 			id: 3,
 			name: 'Поиск платежа без попытки',
-			body: 'Payload: &quot;Кошелек&quot; && Payload: &quot;CreateShopOrderRequest&quot;',
+			body: 'Payload: "Кошелек" && Payload: "CreateShopOrderRequest"',
 			module: 'backend-kassa-main'
 		},
 		
@@ -38,18 +38,36 @@
 		{
 			id: 5,
 			name: 'Поиск займа на карту юзера через наш лайт',
-			body: 'Payload: &quot;XXXXX YYYY&quot; && Payload: (&quot;CreateOrderCommand&quot; &quot;nst_unilabel=&quot; &quot;payerAccount&quot;)',
+			body: 'Payload: "XXXXX YYYY" && Payload: ("CreateOrderCommand" "nst_unilabel=" "payerAccount")',
 			module: 'backend-deposit-main'
 		},	
 	]
 
         const templateSelectBlock = document.createElement('div')
-        templateSelectBlock.classList.add = 'template-select-block'
+        templateSelectBlock.classList.add('template-select-block');
 
-        const templateSelector = document.createElement('select')
-        templateSelector.innerHTML = '<option selected disabled>Выбери шаблон</option>' + templates.filter(x => x.id !== 0)
-          .map(template => `<option value="${template.body}">${template.name}</option>`).join('')
-        const closeSelectorButton = document.createElement('button')
+		const templateSelector = document.createElement('select');
+
+		const options = templates.map(template => {
+			const opt = document.createElement('option');
+
+			opt.data = template;
+			opt.value = template.id;
+			opt.innerText = template.id !== 0
+			  ? template.name
+			  :'Выбери шаблон';
+
+			if (template.id === 0) {
+			  opt.selected = true;
+			  opt.disabled = true;
+			}
+
+			return opt;
+		});
+
+		options.map(opt => templateSelector.appendChild(opt));
+		  
+		const closeSelectorButton = document.createElement('button')
         closeSelectorButton.innerText = '❌'
         closeSelectorButton.title = 'Закрыть'
 
@@ -57,12 +75,12 @@
         templateSelector.style = 'border: 1px solid #8b3ffd; border-radius: 5px 0px 0px 5px; width: 80%; border-right: none; height: 3rem;'
         closeSelectorButton.style = 'width: 19%; border: 1px solid #8b3ffd; border-radius: 0px 5px 5px 0px; border-left: none; cursor: pointer;'
 
-        templateSelector.addEventListener('change', event => {
-          const templateText = new DOMParser().parseFromString(event.target.value.replaceAll('<br />', '\n'), "text/html").documentElement.textContent;
-		  const templateModule = /* нужно сюда передать модуль активного элемента */
-          navigator.clipboard.writeText(templateText)
-          alert('Текст шаблона скопирован в буфер обмена. Модуль: ' + templateModule)
-        })
+		templateSelector.addEventListener('change', event => {
+			const data = templates.find(tmp => tmp.id === +event.target.value);
+			navigator.clipboard.writeText(data.body);
+			alert('Текст шаблона скопирован в буфер обмена. Модуль: ' + data.module);
+		  });
+		  
         closeSelectorButton.addEventListener('click', () => templateSelectBlock.remove())
 
         templateSelectBlock.appendChild(templateSelector)
